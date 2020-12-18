8b79409ad919691b01b9fc83060b75c9dafe6189
repcaK <kacper.tumulec4@gmail.com>
2020-12-18 17:41:59 +0100

    started example

--- populate_rango.py
+++ populate_rango.py
@@ -31,12 +31,20 @@ def populate():
         {"title": "Flask",
          "url": "http://flask.pocco.org"}]
 
-    cats = {"Python": {"pages": python_pages},
-            "Django": {"pages": django_pages},
-            "Other frameworks": {"pages": other_pages}}
+    cats = {"Python": {"pages": python_pages,
+                       "views": 128,
+                       "likes": 64},
+            "Django": {"pages": django_pages,
+                       "views": 64,
+                       "likes": 32},
+            "Other frameworks": {"pages": other_pages,
+                                 "views": 32,
+                                 "likes": 16}}
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        v = cat_data["views"]
+        l = cat_data["likes"]
+        c = add_cat(cat, v, l)
         for p in cat_data["pages"]:
             add_page(c, p["title"], p["url"])
 
@@ -51,8 +59,8 @@ def add_page(cat, title, url, views=0):
     p.views = views
     p.save()
     return p
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_cat(name, views, likes):
+    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
     c.save()
     return c
 
